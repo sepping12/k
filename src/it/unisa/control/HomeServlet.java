e6962e7b7413090387f63a9b38b5f0081bb8188d
espos <espos@172.19.195.102>
2024-05-27 12:25:57 +0200

    id-1 resolved

--- src/it/unisa/control/HomeServlet.java
+++ src/it/unisa/control/HomeServlet.java
@@ -3,6 +3,7 @@ package it.unisa.control;
 import java.io.IOException;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.List;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -19,45 +20,48 @@ import it.unisa.model.ProdottoDao;
  */
 @WebServlet("/home")
 public class HomeServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
- 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-		ProdottoDao dao = new ProdottoDao();
-		
-		ArrayList<ArrayList<ProdottoBean>> categorie = new ArrayList<>();
-		String redirectedPage = request.getParameter("page");
-		
-		try {
-			ArrayList<ProdottoBean> PS5 = dao.doRetrieveByPiattaforma("PlayStation 5");
-			ArrayList<ProdottoBean> XboxSeries = dao.doRetrieveByPiattaforma("Xbox Series");
-			ArrayList<ProdottoBean> Switch = dao.doRetrieveByPiattaforma("Nintendo Switch");
-			ArrayList<ProdottoBean> PS4 = dao.doRetrieveByPiattaforma("PlayStation 4");
-			ArrayList<ProdottoBean> Xone = dao.doRetrieveByPiattaforma("Xbox One");
-			
-			categorie.add(PS5);
-			categorie.add(XboxSeries);
-			categorie.add(Switch);
-			categorie.add(PS4);
-			categorie.add(Xone);
+    private static final long serialVersionUID = 1L;
 
-			request.getSession().setAttribute("categorie", categorie);
-			
+    // Lista di pagine consentite
+    private static final List<String> allowedPages = List.of("home.jsp", "products.jsp", "contact.jsp");
 
-			
-		}catch(SQLException e) {
-			e.printStackTrace();
-		}
-		
-		RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/" + redirectedPage);
-		dispatcher.forward(request, response);
-	}
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
+        ProdottoDao dao = new ProdottoDao();
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-		doGet(request, response);
-	}
+        ArrayList<ArrayList<ProdottoBean>> categorie = new ArrayList<>();
+        String redirectedPage = request.getParameter("page");
 
+        try {
+            ArrayList<ProdottoBean> PS5 = dao.doRetrieveByPiattaforma("PlayStation 5");
+            ArrayList<ProdottoBean> XboxSeries = dao.doRetrieveByPiattaforma("Xbox Series");
+            ArrayList<ProdottoBean> Switch = dao.doRetrieveByPiattaforma("Nintendo Switch");
+            ArrayList<ProdottoBean> PS4 = dao.doRetrieveByPiattaforma("PlayStation 4");
+            ArrayList<ProdottoBean> Xone = dao.doRetrieveByPiattaforma("Xbox One");
+
+            categorie.add(PS5);
+            categorie.add(XboxSeries);
+            categorie.add(Switch);
+            categorie.add(PS4);
+            categorie.add(Xone);
+
+            request.getSession().setAttribute("categorie", categorie);
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        // Verifica che la pagina richiesta sia nella lista delle pagine consentite
+        if (allowedPages.contains(redirectedPage)) {
+            RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/" + redirectedPage);
+            dispatcher.forward(request, response);
+        } else {
+            // Se la pagina non è consentita, reindirizza a una pagina di errore o alla home page
+            response.sendRedirect(request.getContextPath() + "/error.jsp");
+        }
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request, response);
+    }
 }
